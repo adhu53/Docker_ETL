1da0e5fa8bf0cf0544981cd6f7d91101d4e8ed65
adhu53 <82300283+adhu53@users.noreply.github.com>
2026-01-10 17:19:42 +0530

    Add files via upload

---
+++ etl.py
@@ -0,0 +1,119 @@
+import requests
+import datetime
+import csv
+import pymysql
+import time
+
+def pull_file(git_url,today,filename):
+    response=requests.get(git_url)
+    if response.status_code!=200:
+        with open("logs.txt","a") as f1:
+            f1.write(str(datetime.datetime.now())+":"+str(response.raise_for_status)+" File not found error" if response.status_code==404 else " unexpected error")
+            f1.write("\n")
+            return False
+    with open(filename,"wb") as f:
+        for data in response.iter_content(chunk_size=64):
+            if data:
+                f.write(data)
+    return True
+
+def update_sql_db(eid,ename,esal,ecity,mod):
+    while True:
+        try:
+            conn=pymysql.connect(host="mysql",database="jan",user="root",password="root")
+            cursor=conn.cursor()
+            cursor.execute("create table if not exists employee_records (eid int primary key, ename varchar(10),esal float, ecity varchar(10))")
+            if mod=="A":
+                if cursor.execute("select eid from employee_records where eid=%s",eid):
+                    print("entry alredy exist")
+                    return
+                sql="insert into employee_records (eid,ename,esal,ecity) values (%s, %s, %s, %s)"
+                cursor.execute(sql, (eid,ename,esal,ecity))
+                conn.commit()
+                break
+            elif mod=="D":
+                sql="delete from employee_records where eid=%s"
+                cursor.execute(sql,eid)
+                conn.commit()
+                break
+            elif mod=="C":
+                sql="update employee_records set ename=%s, esal=%s, ecity=%s where eid=%s"
+                cursor.execute(sql,(ename,esal,ecity,eid))
+                conn.commit()
+                break
+            else:
+                print("The file is corrupt")
+                break
+        except Exception as e:
+            print(e)
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                conn.close()
+
+
+def process_file(filename):
+    flag=0
+    with open (filename, "r",encoding="UTF-8") as f2:
+        r=csv.reader(f2)
+        next(r) # skipping header
+        datas=list(r)
+        for data in datas:
+            for item in map(lambda x: x.split(","),data): #data will be in format ["101,adarsh,1000,bangalore"]
+                mod=str(item[0]).strip()
+                eid=int(item[1])
+                ename=str(item[2]).strip()
+                esal=float(item[3])
+                ecity=str(item[4]).strip()
+            if eid==None or (len(str(eid))<2 and len(str(eid))>6):
+                flag=1
+            elif ename==None or len(ename)>10:
+                flag=1
+            elif esal==None or esal<0:
+                flag=1
+            elif ecity==None or  len(ecity)>10:
+                flag=1
+            if flag==1:
+                print("data in file is not correct:",eid,ename,esal,ecity)
+            else:
+                update_sql_db(eid,ename,esal,ecity,mod)
+
+
+def main(last_processed_date,today,git_url,filename):
+    while True:
+        today=datetime.datetime.now().strftime("%Y-%m-%d")
+        if last_processed_date==None:
+            print("file processing for the first time")
+            pull_status=pull_file(git_url,today,filename)
+            if pull_status==False:
+                print("Processing failed, check log for more details")
+            elif pull_status==True:
+                process_file(filename)
+                last_processed_date=today
+        elif last_processed_date==today:
+            print("File already processed for today")
+            last_processed_date=today
+        else:
+            pull_status=pull_file(git_url,today,filename)
+            if pull_status==False:
+                print("Processing failed, check log for more details")
+            elif pull_status==True:
+                process_file(filename)
+                last_processed_date=today
+         
+        time.sleep(10)    
+        
+
+#get the name of the file to be processed
+git_url="https://raw.githubusercontent.com/adhu53/Docker_ETL/refs/heads/main/employees_"
+today=datetime.datetime.now().strftime("%Y-%m-%d")
+git_url=git_url+today+".csv"
+print(today)
+filename="employees_"+today+".csv"
+print(filename)
+last_processed_date=None
+
+if __name__=="__main__":
+    main(last_processed_date,today,git_url,filename)
+    
